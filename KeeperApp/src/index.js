a340c647dc06b2876b1579125cdab3e5518f960e
Esther-T <tan.esther8@gmail.com>
2021-06-20 12:47:25 -0700

    Work on Keeper App

---
+++ KeeperApp/src/index.js
@@ -0,0 +1,9 @@
+//1. Create a new React app.
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App.jsx"
+
+ReactDOM.render(
+  <App />,
+  document.getElementById("root")
+);
