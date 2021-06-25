ea652c3565dc3a37ebed4c930afe95b7de3ef8a4
Esther-T <tan.esther8@gmail.com>
2021-06-24 17:45:01 -0700

    Playing with React forms

--- LoginSystem/src/components/Form.jsx
+++ LoginSystem/src/components/Form.jsx
@@ -3,7 +3,9 @@ import React, { useState } from "react";
 
 function Form(props) {
   const [isMousedOver, setMouseOver] = useState(false);
-  const [headingText, setHeadingText] = useState("Click on the Login button");
+  const [headingText, setHeadingText] = useState("Hello");
+  const [username, setUsername] = useState("");
+
   function handleMouseOver(){
     setMouseOver(true);
   }
@@ -12,22 +14,31 @@ function Form(props) {
     setMouseOver(false);
   }
 
-  function handleLoginClick(){
-    setHeadingText("Clicked!");
+  function handleClick(event){
+    console.log(username);
+    setHeadingText("Welcome " + username);
+    event.preventDefault();
+  }
+
+  function handleChange(event){
+    setUsername(event.target.value);
   }
 
   return (
-    <form className="form">
+    <form className="form" onSubmit={handleClick}>
       <h1>{headingText}</h1>
-      <input type="text" placeholder="Username" />
+      <input type="text"
+      name="Username"
+      placeholder="Username"
+      onChange={handleChange}
+      value={username}/>
       <input type="password" placeholder="Password" />
       {!props.registered && <input type="password" placeholder="Confirm Password" />}
       <button
-      type="button"
+      type="submit"
       style={{backgroundColor: isMousedOver ? "black" : "white"}}
       onMouseOver={handleMouseOver}
-      onMouseOut={handleMouseOut}
-      onClick={handleLoginClick}>
+      onMouseOut={handleMouseOut}>
       {props.registered ? "Login" : "Registered"}</button>
     </form>
   );
