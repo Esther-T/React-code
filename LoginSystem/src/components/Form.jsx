817292d1853a6bd8aa98bf8ed77248910e9dcdc2
Esther-T <tan.esther8@gmail.com>
2021-06-24 17:13:36 -0700

    Work on handling events

--- LoginSystem/src/components/Form.jsx
+++ LoginSystem/src/components/Form.jsx
@@ -1,12 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
+
 
 function Form(props) {
+  const [isMousedOver, setMouseOver] = useState(false);
+  const [headingText, setHeadingText] = useState("Click on the Login button");
+  function handleMouseOver(){
+    setMouseOver(true);
+  }
+
+  function handleMouseOut(){
+    setMouseOver(false);
+  }
+
+  function handleLoginClick(){
+    setHeadingText("Clicked!");
+  }
+
   return (
     <form className="form">
+      <h1>{headingText}</h1>
       <input type="text" placeholder="Username" />
       <input type="password" placeholder="Password" />
       {!props.registered && <input type="password" placeholder="Confirm Password" />}
-      <button type="submit">{props.registered ? "Login" : "Registered"}</button>
+      <button
+      type="button"
+      style={{backgroundColor: isMousedOver ? "black" : "white"}}
+      onMouseOver={handleMouseOver}
+      onMouseOut={handleMouseOut}
+      onClick={handleLoginClick}>
+      {props.registered ? "Login" : "Registered"}</button>
     </form>
   );
 }
