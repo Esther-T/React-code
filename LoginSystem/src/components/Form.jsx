bbe2d986fbe34f8a97e0b3cf86bbd7ca818edbd9
Esther-T <tan.esther8@gmail.com>
2021-06-25 22:34:28 -0700

    Work on complex states and spread operators

--- LoginSystem/src/components/Form.jsx
+++ LoginSystem/src/components/Form.jsx
@@ -4,7 +4,12 @@ import React, { useState } from "react";
 function Form(props) {
   const [isMousedOver, setMouseOver] = useState(false);
   const [headingText, setHeadingText] = useState("Hello");
-  const [username, setUsername] = useState("");
+  const [user, setUser] = useState({
+    fName : "",
+    lName : "",
+    email : ""
+  });
+
 
   function handleMouseOver(){
     setMouseOver(true);
@@ -15,31 +20,51 @@ function Form(props) {
   }
 
   function handleClick(event){
-    console.log(username);
-    setHeadingText("Welcome " + username);
+    //console.log(fName);
+    //setHeadingText("Welcome " + fName + " " + lName);
     event.preventDefault();
   }
 
   function handleChange(event){
-    setUsername(event.target.value);
+    const newValue = event.target.value;
+    const inputName = event.target.name;
+
+    setUser(prevValue => {
+      return {
+        ...prevValue,
+        [inputName]: newValue
+      }
+    });
   }
 
   return (
     <form className="form" onSubmit={handleClick}>
-      <h1>{headingText}</h1>
+      <h1>Hello {user.fName} {user.lName}</h1>
+      <p>{user.email}</p>
       <input type="text"
-      name="Username"
-      placeholder="Username"
+      name="fName"
+      placeholder="First Name"
       onChange={handleChange}
-      value={username}/>
-      <input type="password" placeholder="Password" />
-      {!props.registered && <input type="password" placeholder="Confirm Password" />}
+      value={user.fName}
+      />
+      <input type="text"
+      name="lName"
+      placeholder="Last Name"
+      value={user.lName}
+      onChange={handleChange}
+      />
+      <input type="text"
+      name="email"
+      placeholder="Email"
+      value={user.email}
+      onChange={handleChange}/>
+
       <button
       type="submit"
       style={{backgroundColor: isMousedOver ? "black" : "white"}}
       onMouseOver={handleMouseOver}
       onMouseOut={handleMouseOut}>
-      {props.registered ? "Login" : "Registered"}</button>
+      Submit</button>
     </form>
   );
 }
